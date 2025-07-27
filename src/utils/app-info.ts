1f633ec45b4d1c1978038a561b85d0f7cf67b07d
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2025-07-27 20:11:25 -0300

    Add Header component and app info, update layout and index page

---
+++ src/utils/app-info.ts
@@ -0,0 +1,8 @@
+export const appInfo = {
+  title: 'Emoji Copy',
+  description:
+    'Uma extensão simples do Chrome para copiar emojis para a área de transferência',
+  keywords: 'emoji, copiar, extensão, chrome',
+  url: 'https://chromewebstore.google.com/detail/fnichdelhncpnocohahjklimdlmjjeij?utm_source=item-share-cb',
+  repository: 'https://github.com/ClodoaldoDantas/emoji-copy',
+}
